685a4444b2b88871f0fe834c5bcb6940cc9a2f53
Mr Toryk <ilya.galchinskyi@gmail.com>
2024-04-27 17:26:16 +0300

    1

--- ServerLibs/WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ ServerLibs/WebAPI/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94c80c0b00ca83e3f26201ea2d7cb16de6921608")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9507ac3a4d1455b322ef34238dc11d710906df88")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
